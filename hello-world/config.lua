781ddc6ed4c7e6f24b20f3d42cbb8139c350c25f
ximenpo <ximenpo@jiandan.ren>
2015-05-10 23:40:27 +0800

    hello-world 项目可以正常执行。

--- hello-world/config.lua
+++ hello-world/config.lua
@@ -1,9 +1,15 @@
 
 ----------------------------------
+--  局部变量
+----------------------------------
+local _root		= "./"
+local _skynet	= _root.."skynet/"
+
+----------------------------------
 --  自定义参数
 ----------------------------------
-app_root    = "./"
-app_name    = "hello-world"
+app_name    	= "hello-world"
+app_root    	= _root..app_name.."/"
 
 ----------------------------------
 --  skynet用到的六个参数
@@ -19,7 +25,7 @@ app_name    = "hello-world"
 --  工作线程数
 thread      = 2
 --  服务模块路径（.so)
-cpath       = app_root.."skynet/cservice/?.so"
+cpath       = _skynet.."cservice/?.so"
 --  港湾ID，用于分布式系统，0表示没有分布
 harbor      = 0
 --  后台运行用到的 pid 文件
@@ -32,7 +38,7 @@ bootstrap   = "snlua hello-world"
 ----------------------------------
 --  snlua用到的参数
 ----------------------------------
-lua_path    = nil
-lua_cpath   = nil
+lua_path    = _skynet.."lualib/?.lua;"..app_root.."/?.lua"
+lua_cpath   = _skynet.."luaclib/?.so;"..app_root.."/?.so"
 lualoader   = "skynet/lualib/loader.lua"
-luaservice  = app_root.."skynet/service/?.lua;"..app_root..app_name.."/?.lua"
+luaservice  = _skynet.."service/?.lua;"..app_root.."/?.lua"
