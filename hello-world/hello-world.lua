d352a50f9b28737c4c8eddbf2f838b5cf20a6476
ximenpo <ximenpo@jiandan.ren>
2015-05-11 18:49:41 +0800

    hello-world 例子 增加为两种实现方式

--- hello-world/hello-world.lua
+++ hello-world/hello-world.lua
@@ -1,19 +1,33 @@
 local skynet    = require "skynet"
 local core      = require "skynet.core"
 
+--方式1
+--[[
+skynet.start(function()
+    --  send message to logger
+    skynet.error("hello, world!")
+
+    --  dalay and quit
+    skynet.timeout(1, skynet.abort)
+end)
+--]]
+
+--方式2
+--[－[
 skynet.start(function()
     -- get the logger service
     local logger    = skynet.localname(".logger")
     if not logger then
         skynet.error("no logger found")
+        skynet.abort()
     end
 
-    --  send message to logger
     core.send(logger, skynet.PTYPE_TEXT, 0, "hello, world!")
 
     --  delay quit for the logger display
-    skynet.timeout(0, function()
+    skynet.timeout(1, function()
         skynet.abort()
         --skynet.exit()
     end)
 end)
+--]]
