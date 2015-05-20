86ae3ed49c579bd9bb97c36c46a8cca770f7b3c6
ximenpo <ximenpo@jiandan.ren>
2015-05-20 15:02:00 +0800

    统一新项目用到main文件。

--- hello-socketchannel/main.lua
+++ hello-socketchannel/main.lua
@@ -5,6 +5,7 @@ local snax      = require "snax"
 skynet.start(function()
     --初始化随机数
     math.randomseed( tonumber(tostring(os.time()):reverse():sub(1,6)) )
+    
     --启动console服务，并命名为 .console
     skymgr.name(".console", skynet.newservice("console"))
     --启动debug_console服务，并命名为 .dbgconsole
